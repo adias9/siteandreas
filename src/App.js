8d1f9ab3718eb51044cbbdeb9f494051a49e5e9f
adias9 <andreasdias9@gmail.com>
2018-11-15 18:52:14 -0600

    Beginning react personal website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navigation />
+        <div className="jumbotron">
+          <h1 className="jumbotron-header" style="font-family: Hiragino Kaku Gothic; font-weight: 700; padding-top: 20vh">andreas dias</h1>
+          <p className="jumbotron-body">Technologist, <span role="img" aria-label="soccer_ball">⚽ </span> fan, and design enthusiast. <br /> Constantly working on creating the future.</p>
+          <a href="mailto:andreasdias9@gmail.com" target="_blank" rel='noopener noreferrer'>email</a>
+          <a href="https://github.com/adias9" target="_blank" rel='noopener noreferrer'>github</a>
+          <a href="https://linkedin.com/in/andreas-dias/" target="_blank" rel='noopener noreferrer'>linkedin</a>
+          <a href="/">resume</a>
+        </div>
       </div>
     );
   }
