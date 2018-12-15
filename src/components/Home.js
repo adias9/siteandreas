6e9fc55d7b1fb359059606e2214b9623e1b1c473
adias9 <andreasdias9@gmail.com>
2018-12-14 21:06:39 -0500

    add resume pdf

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import VRScene from './VRScene.js'
 import '../css/vrscene.scss'
+import pdf from '../Andreas_Tech_Resume.pdf'
 
 const Home = () => (
     <div className="">
@@ -11,7 +12,7 @@ const Home = () => (
                 <p><a href="mailto:andreasdias9@gmail.com" target="_blank" rel='noopener noreferrer'>email</a></p>
                 <p><a href="https://github.com/adias9" target="_blank" rel='noopener noreferrer'>github</a></p>
                 <p><a href="https://linkedin.com/in/andreas-dias/" target="_blank" rel='noopener noreferrer'>linkedin</a></p>
-                <p><a href="/">resume</a></p>
+                <p><a href={pdf}>resume</a></p>
             </div>
         </div>
         <VRScene/>
